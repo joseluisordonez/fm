1f82ec5eed7bc581c7e6c819497395660f50ddac
joseluisordonez <pepe_2597@hotmail.com>
2015-03-19 23:21:31 -0700

    pruebas con rutas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,3 +19,9 @@ Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+
+Route::get('/bar/{nom}/{ap?}', function($nom,$ap=null)
+{
+    return 'Hola '.$nom.' '.$ap;
+})-> where ('nom','[a-z]+');
+
